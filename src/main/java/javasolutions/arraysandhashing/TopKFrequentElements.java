d482243564052faab43945f129134624f6c449de
matcinks <x.mateuszstepien@gmail.com>
2024-03-01 19:54:11 +0100

    Solved same task using Java: 4. Top K frequent elements.

--- src/main/java/javasolutions/arraysandhashing/TopKFrequentElements.java
+++ src/main/java/javasolutions/arraysandhashing/TopKFrequentElements.java
@@ -1,20 +1,21 @@
 package javasolutions.arraysandhashing;
 
+import com.sun.source.tree.Tree;
 import javasolutions.LeetCodeTask;
 
+import java.util.*;
+
 /**
- *
  * Given an integer array nums and an integer k, return the k most frequent elements.
  * You may return the answer in any order.
- *
+ * <p>
  * Example 1:
  * Input: nums = [1,1,1,2,2,3], k = 2
  * Output: [1,2]
- *
+ * <p>
  * Example 2:
  * Input: nums = [1], k = 1
  * Output: [1]
- *
  */
 
 public class TopKFrequentElements extends LeetCodeTask {
@@ -24,7 +25,29 @@ public class TopKFrequentElements extends LeetCodeTask {
     }
 
     public int[] topKFrequent(int[] nums, int k) {
-        return null;
-    }
+        Map<Integer, Integer> calcMap = new HashMap<>();
+        List<Integer>[] appearancesBucket = new ArrayList[nums.length + 1];
+        int[] result = new int[k];
 
+        for (int n : nums) {
+            calcMap.put(n, calcMap.getOrDefault(n, 0) + 1);
+        }
+
+        for (int key : calcMap.keySet()) {
+            int freq = calcMap.get(key);
+            if(appearancesBucket[freq] == null) {
+                appearancesBucket[freq] = new ArrayList<>();
+            }
+            appearancesBucket[freq].add(key);
+        }
+
+        for (int i = nums.length, counter = 0; i>=0 && counter < k; i--) {
+            if (appearancesBucket[i] != null) {
+                for (int val: appearancesBucket[i]) {
+                    result[counter++] = val;
+                }
+            }
+        }
+        return result;
+    }
 }
